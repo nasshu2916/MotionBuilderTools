a946696512172b2cdcfd618999bf25359f9849e5
naoya kohda <naoya.kohda@gmail.com>
2023-12-17 02:32:18 +0900

    キャメルケースや suffix の場合に対応させる

--- Scripts/MBTools/set_bone_angle.py
+++ Scripts/MBTools/set_bone_angle.py
@@ -3,6 +3,7 @@
 
 import glob
 import os
+import re
 import sys
 import yaml
 
@@ -28,32 +29,49 @@ def load_yaml(file_path):
     return yaml_data
 
 
-def execute(root_bone, target_config, prefix=""):
+def normalize_string(s):
+    """
+    文字列を正規化する
+    アンダースコアを取り除き、単語の先頭を大文字のキャメルケースに変換する
+    """
+    s = s.replace("-", "_")
+    return re.sub(r'_([a-z])', lambda m: m.group(1).upper(), s)
+
+
+def execute(root_bone, target_config):
     bones = [root_bone]
     bones.extend(get_children(root_bone))
     FBSystem().Scene.Evaluate()
-
     items = target_config["items"]
-    for prefix in ["", "Left", "Right"]:
-        is_reverse = False
-        if prefix != "":
-            is_reverse = target_config.get("reverse_{}".format(prefix.lower()), False)
 
-        __do_execute(bones, items, is_reverse, prefix, "")
-        FBSystem().Scene.Evaluate()
+    __do_execute(bones, items)
 
-def __do_execute(bones, items, is_reverse, prefix = "", suffix = ""):
+    for affix in ["Left", "Right"]:
+        is_reverse = target_config.get("reverse_{}".format(affix.lower()), False)
+
+        if is_reverse == False:
+            # reverse が指定されていない場合は affix なしで回転済みなのでスキップする
+            print("skip: {}".format(affix))
+            continue
+
+        # affix の頭文字も実行対象にする
+        for execute_affix in [affix, affix[0]]:
+            __do_execute(bones, items, is_reverse, execute_affix, "")
+            __do_execute(bones, items, is_reverse, "", execute_affix)
+    FBSystem().Scene.Evaluate()
+
+
+def __do_execute(bones, items, is_reverse=False, prefix="", suffix=""):
     for item in items:
-        search_name = prefix + item["key"].strip(prefix).strip(suffix) + suffix
-        bone = None
+        search_name = prefix + item["key"] + suffix
+        search_name = normalize_string(search_name)
+        # 実行対象の bone を取得する
+        bone = __search_bone(bones, search_name)
 
-        for bone in bones:
-            bone_name = bone.Name
-            if bone.Name.endswith(search_name):
-                bone = bone
-                break
         if bone:
             value = item["value"]
+            # value に x, y, z が指定されている場合はその値を設定する
+            # null や指定されていない場合は現在の値を設定する
             rotation = FBVector3d(
                 value["x"] if value.get("x") is not None else bone.Rotation[0],
                 value["y"] if value.get("y") is not None else bone.Rotation[1],
@@ -62,11 +80,20 @@ def __do_execute(bones, items, is_reverse, prefix = "", suffix = ""):
 
             # is_reverse が True の場合は z 軸の回転を反転させる
             if is_reverse: rotation[2] *= -1
-
             bone.SetVector(rotation, FBModelTransformationType.kModelRotation, True)
 
-# 再帰的に node の children を取得する
+
+def __search_bone(bones, search_name):
+    "search_name に一致する bone を取得する"
+
+    for bone in bones:
+        if normalize_string(bone.Name).endswith(search_name):
+            return bone
+
+
 def get_children(root):
+    "再帰的に node の children を取得する"
+
     children = []
     for child in root.Children:
         children.append(child)
@@ -85,7 +112,7 @@ def set_bone_angle(template_path):
     if len(set([id(s) for s in root_bones])) == 1:
         root_bone = root_bones[0]
         yaml_data = load_yaml(template_path)
-        execute(root_bone, yaml_data["target"], "")
+        execute(root_bone, yaml_data["target"])
 
     else:
         FBMessageBox("Warning", "Please Select Actor's bone.", "OK")
